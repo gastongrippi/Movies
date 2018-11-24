5446b6f9d447c3ef96a1cecf813d3377aee9507d
Gaston Grippi <gaston.daniel.grippi@fisglobal.com>
2018-11-24 11:01:59 -0300

    setup inicial para aplicacion de peliculas

---
+++ Movies/Movies/listOfMoviesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  listOfMoviesTableViewController.h
+//  Movies
+//
+//  Created by gaston on 11/24/18.
+//  Copyright © 2018 gaston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface listOfMoviesTableViewController : UITableViewController
+
+@end
